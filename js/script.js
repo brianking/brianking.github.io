ce53ccf8bf9965ed6d93bb110e71d046939a3c94
Brian King <briks.si@gmail.com>
2017-05-12 17:06:59 +0200

    New homepage associated files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function hoverProfileImage(element) {
+    element.setAttribute("src", "img/icon.png");
+}
+function unhoverProfileImage(element) {
+    element.setAttribute("src", "img/profile-image.jpg");
+}
